03a4e3bb163d9ed2c471683cfd67ce5ee94cb2a5
Akira9701 <akira.zzz97@gmail.com>
2023-04-13 13:53:39 +0300

    first test auto build

--- src/geometry/main.cpp
+++ src/geometry/main.cpp
@@ -29,22 +29,19 @@ int main()
                 str[c] == '(' ? bracketsStatus = 1 : bracketsStatus = 0;
                 c++;
             } else if (name.compare("triangle") == 0) {
-                str[c] == '(' && str[c + 1] == '(' ? bracketsStatus = 2
-                                                   : bracketsStatus = 0;
+                str[c] == '(' && str[c + 1] == '(' ? bracketsStatus = 2 : bracketsStatus = 0;
                 c += 2;
             } else {
                 std::cout << str << std::endl;
 
-                std::cout << "Error at column 0: expected 'circle', 'triangle'"
-                          << std::endl;
+                std::cout << "Error at column 0: expected 'circle', 'triangle'" << std::endl;
                 exit(0);
             }
 
             if (bracketsStatus == 0) {
                 std::cout << str << std::endl;
 
-                std::cout << "Error at column " << c << ": expected '('"
-                          << std::endl;
+                std::cout << "Error at column " << c << ": expected '('" << std::endl;
                 exit(0);
             }
             pointsCount = checkPoints(pt);
@@ -55,8 +52,7 @@ int main()
                 do {
                     if (str[c] >= 'a' && str[c] <= 'z') {
                         std::cout << str << std::endl;
-                        std::cout << "Error at column " << c
-                                  << ": expected '<double>'" << std::endl;
+                        std::cout << "Error at column " << c << ": expected '<double>'" << std::endl;
                         exit(0);
                     }
                     if (!(str[c] == ' ' && str[c - 1] == ',')) {
@@ -72,17 +68,14 @@ int main()
                 if (str[c] != ')') {
                     std::cout << str << std::endl;
 
-                    std::cout << "Error at column " << c << ": expected ) "
-                              << std::endl;
+                    std::cout << "Error at column " << c << ": expected ) " << std::endl;
                     exit(0);
                 }
             }
             if (name.compare("triangle") == 0) {
                 if (pointsArray[0].compare(pointsArray[pointsCount]) != 0) {
                     std::cout << str << std::endl;
-                    std::cout << "Error at column " << c
-                              << " first and last points not same "
-                              << std::endl;
+                    std::cout << "Error at column " << c << " first and last points not same " << std::endl;
                     exit(0);
                 }
             }
